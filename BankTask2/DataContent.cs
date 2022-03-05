6ab34c1d3ade53096efe21177c4555f4225835cf
Lempi-sudo <andreii.lapschow1999@mail.ru>
2022-03-05 23:15:25 +0400

    Добавил Model(DataContent) интерфейсы для считывание csv и интерфейс для парсинга данных

---
+++ BankTask2/DataContent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTask2
+{
+    class DataContent
+    {
+        private List<DataContent> _data;
+
+        public  List<DataContent> Data 
+        {   
+            get 
+            {
+                return _data ; 
+            } 
+
+            private set
+            {
+                _data = value;
+            }
+        }
+
+        private int maxYearEmployee;
+        private int minYearEmployee;
+
+        public double meanYearsEmployee()
+        {
+            double tmp = (maxYearEmployee - minYearEmployee) / 2;
+            return maxYearEmployee -tmp;
+        }
+
+        public DataContent(List<DataContent> data)
+        {
+            Data = data;
+        }
+
+    }
+}
+
+    
+    
---
+++ BankTask2/Employee.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTask2
+{
+    enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+
+
+    class Employee
+    {
+
+        public string Name { get; set; }
+        public int BirthYears { get; set; }
+
+        private int _age;
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                int currentYear = DateTime.Now.Year;
+                _age = currentYear - value;
+            }
+        }
+
+
+       
+
+
+        private int _yearBeforPension;
+        public int YearBeforePension
+        {
+            get
+            {
+                return _yearBeforPension;
+            }
+            private set
+            {
+                _yearBeforPension = value;
+            }
+
+        }
+
+        private Gender gender;
+
+        public Employee(string Name, int BirthYears)
+        {
+            this.Name = Name;
+            this.BirthYears = BirthYears;
+            this.Age = BirthYears;
+            this.gender = determineGenderByName(this.Name);
+        }
+
+        public int CalculateHowManyYearsUntilPension(int maleageuntilpension, int femaleageuntilpension)
+        {
+            if (gender == Gender.Female)
+            {
+                return femaleageuntilpension - this.Age;
+            }
+            else if (gender == Gender.Male)
+            {
+                return maleageuntilpension - this.Age;
+            }
+            else
+            {
+                Console.WriteLine("Информации для такого гендера нет");
+                return 0;
+            }
+        }
+
+        public Gender determineGenderByName(string Name)
+        {
+            return Gender.Other;
+
+        } 
+
+
+
+    }
+}
